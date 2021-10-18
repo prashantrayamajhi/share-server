d3dcfb1a854830aad25e9b7b41c79319c0286c2c
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2021-10-18 15:48:27 +0545

    get lated post in order

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -2,7 +2,9 @@ const Post = require("./../model/Post");
 
 exports.getPosts = async (req, res) => {
   try {
-    const data = await Post.find().populate("user", "name");
+    const data = await Post.find().populate("user", "name").sort({
+      createdAt: -1,
+    });
     return res.status(200).json({ data });
   } catch (err) {
     console.log(err);
--- controllers/users/post.controller.js
+++ controllers/users/post.controller.js
@@ -2,7 +2,9 @@ const Post = require("./../../model/Post");
 
 exports.getPosts = async (req, res) => {
   try {
-    const data = await Post.find({ user: req.user._id });
+    const data = await Post.find({ user: req.user._id }).sort({
+      createdAt: -1,
+    });
     return res.status(200).json({ data });
   } catch (err) {
     console.log(err);
