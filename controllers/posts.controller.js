b841a756910ca51fa8abef8d0ff8876cb27ae96f
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2022-01-18 20:52:25 +0545

    remove file unlink code

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -43,40 +43,40 @@ exports.getPostById = async (req, res) => {
 };
 
 exports.createPost = async (req, res) => {
-  cloudinaryConfig();
-  let { title, content, private, description } = req.body;
-  if (!title || title.trim().length <= 0) {
-    return res.status(400).send({ err: "Title cannot be empty" });
-  }
-  if (!description || description.trim().length <= 0) {
-    return res.status(400).send({ err: "Description cannot be empty" });
-  }
-  if (!content || content.trim().length <= 0) {
-    return res.status(400).send({ err: "Content cannot be empty" });
-  }
-
-  if (!req.files || req.files.length <= 0) {
-    return res.status(400).json({ err: "Missing Images" });
-  }
-
-  const images = [];
-  const imgKeys = Object.keys(req.files);
-  imgKeys.forEach((key) => {
-    images.push(req.files[key]);
-  });
-
-  const publicId = [];
-  const imageArr = [];
-  for (let img of images) {
-    const path = img.path;
-    const upload = await cloudinary.v2.uploader.upload(path);
-
-    fs.unlinkSync(path);
-    publicId.push(upload.public_id);
-    imageArr.push(upload.secure_url);
-  }
-
   try {
+    cloudinaryConfig();
+    let { title, content, private, description } = req.body;
+    if (!title || title.trim().length <= 0) {
+      return res.status(400).send({ err: "Title cannot be empty" });
+    }
+    if (!description || description.trim().length <= 0) {
+      return res.status(400).send({ err: "Description cannot be empty" });
+    }
+    if (!content || content.trim().length <= 0) {
+      return res.status(400).send({ err: "Content cannot be empty" });
+    }
+
+    if (!req.files || req.files.length <= 0) {
+      return res.status(400).json({ err: "Missing Images" });
+    }
+
+    const images = [];
+    const imgKeys = Object.keys(req.files);
+    imgKeys.forEach((key) => {
+      images.push(req.files[key]);
+    });
+
+    const publicId = [];
+    const imageArr = [];
+    for (let img of images) {
+      const path = img.path;
+      const upload = await cloudinary.v2.uploader.upload(path);
+
+      fs.unlinkSync(path);
+      publicId.push(upload.public_id);
+      imageArr.push(upload.secure_url);
+    }
+
     const post = new Post({
       title,
       content,
@@ -112,7 +112,7 @@ exports.updatePost = async (req, res) => {
     for (let img of images) {
       const path = img.path;
       const upload = await cloudinary.v2.uploader.upload(path);
-      fs.unlinkSync(path);
+      // fs.unlinkSync(path);
       publicId.push(upload.public_id);
       imageArr.push(upload.secure_url);
     }
