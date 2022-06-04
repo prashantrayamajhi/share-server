be8fdfa0fd2ba945cce16e986a6cafa511102632
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2022-06-04 22:31:38 +0545

    fix nodemailer mail problem

--- helper/mail.js
+++ helper/mail.js
@@ -1,15 +1,5 @@
 const nodemailer = require("nodemailer");
-
-/**
- * Creates transporter object that will help us to send emails
- */
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-});
+const { google } = require("googleapis");
 
 /**
  *  Sends an email to user
@@ -19,7 +9,34 @@ const transporter = nodemailer.createTransport({
  * @param {string} html content of the email
  */
 exports.sendEmail = async (to, subject, html) => {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.OAUTH_CLIENTID,
+    process.env.OAUTH_CLIENT_SECRET,
+    process.env.REDIRECT_URI
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.OAUTH_REFRESH_TOKEN,
+  });
+
   try {
+    const accessToken = await oauth2Client.getAccessToken();
+    /**
+     * Creates transporter object that will help us to send emails
+     */
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+        accessToken,
+      },
+    });
+
     const options = { from: process.env.MAIL_USER, to, subject, html };
     const mail = await transporter.sendMail(options);
     return mail;
@@ -38,7 +55,33 @@ exports.sendEmail = async (to, subject, html) => {
  */
 
 exports.sendVerificationToken = async ({ to, subject, html }) => {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.OAUTH_CLIENTID,
+    process.env.OAUTH_CLIENT_SECRET,
+    process.env.REDIRECT_URI
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.OAUTH_REFRESH_TOKEN,
+  });
+
   try {
+    const accessToken = await oauth2Client.getAccessToken();
+    /**
+     * Creates transporter object that will help us to send emails
+     */
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+        accessToken,
+      },
+    });
     const options = { from: process.env.MAIL_USER, to, subject, html };
     const mail = await transporter.sendMail(options);
     return mail;
